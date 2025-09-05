e7010a9cc59afde0c698f50ed2703b0cc0abcb9e
eden chazard <neon.fortytwo@gmail.com>
2025-09-05 16:01:26 +0100

    rm examples

--- eslint-config/examples/complete.js
+++
@@ -1,5 +0,0 @@
-// Complete configuration (Vue + TypeScript + Vitest)
-// This is equivalent to the original eslint.config.js
-import config from "@synergitech/eslint-config";
-
-export default config;
